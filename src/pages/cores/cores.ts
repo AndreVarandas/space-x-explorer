9fb67d1210a8c7f334ec525c2bd9e78fe41b67af
André Varandas <andre.m.varandas@gmail.com>
2018-10-08 01:56:35 +0100

    fix(launch dates): future launches should display hours

--- src/pages/cores/cores.ts
+++ src/pages/cores/cores.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Observable } from 'rxjs/Observable';
 import { CacheService, Cache } from 'ionic-cache-observable';
 
